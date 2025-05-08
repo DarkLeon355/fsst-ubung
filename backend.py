79771d21b7758458afc49c9b256f6afa7b818963
dLeon_ <leon.maier@live.at>
2025-05-08 10:46:49 +0200

    created backend.py

---
+++ backend.py
@@ -0,0 +1,27 @@
+import random as r
+
+class Zahlenraten:
+    def __init__(self, min: int = 1, max: int = 10):
+        self.min = min
+        self.max = max
+        self.versuche = 0
+        self.random_number()
+
+    def random_number(self):
+        self.zahl = r.randint(self.min, self.max)
+
+    def raten(self, user_input):
+        user_input = int(user_input)
+        self.versuche += 1
+        if user_input == self.zahl: 
+            self.random_number()
+            return 0 # richtig
+        if user_input < self.zahl: return -1 # zu klein
+        if user_input > self.zahl: return 1 # zu groß
+        
+        
+
+Zahlenraten = Zahlenraten()
+while True:
+    print(Zahlenraten.raten(int(input("Number input:"))))
+    
